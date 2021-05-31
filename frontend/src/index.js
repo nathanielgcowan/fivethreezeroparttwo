c52259db2e57e99720f6ea5809d85f511aeccb6c
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-05-31 07:35:37 -0500

    start of creature

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,10 @@
-console.log("hello")
+const cREATURES_URL = "http://localhost:3000/creatures"
+// skills URL 
+const makeACreature = document.querySelector(".makeacreature")
+const enterCreatureName = document.querySelector(".creaturename")
+const enterCreatureImage = document.querySelector(".creatureimage")
+const enterCreatureDescription = document.querySelector(".creaturedescription")
+const deck = document.querySelector(".deck")
+
+makeACreature.addEventListener("submit", Creature.submitCreature)
+Creature.fetchCreatures()
